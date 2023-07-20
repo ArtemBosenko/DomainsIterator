e1bec06873ee5c272fe4f0b755124ad8ac08ff51
Tioma BOSENKO <tiomabosenko@o-s-i.org>
2023-07-21 01:08:11 +0300

    Added relation between the Url and the Data

--- src/Entity/URL.php
+++ src/Entity/URL.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\URLRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: URLRepository::class)]
@@ -19,6 +21,14 @@ class URL
     #[ORM\ManyToOne(inversedBy: 'urls')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Domain $domain = null;
+
+    #[ORM\OneToMany(mappedBy: 'url', targetEntity: Data::class, orphanRemoval: true)]
+    private Collection $url_data;
+
+    public function __construct()
+    {
+        $this->url_data = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +55,36 @@ class URL
     public function setDomain(?Domain $domain): static
     {
         $this->domain = $domain;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Data>
+     */
+    public function getUrlData(): Collection
+    {
+        return $this->url_data;
+    }
+
+    public function addUrlData(Data $urlData): static
+    {
+        if (!$this->url_data->contains($urlData)) {
+            $this->url_data->add($urlData);
+            $urlData->setUrl($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUrlData(Data $urlData): static
+    {
+        if ($this->url_data->removeElement($urlData)) {
+            // set the owning side to null (unless already changed)
+            if ($urlData->getUrl() === $this) {
+                $urlData->setUrl(null);
+            }
+        }
 
         return $this;
     }
