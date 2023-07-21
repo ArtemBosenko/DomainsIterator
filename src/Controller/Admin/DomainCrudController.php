d9f8f627e1614967e80fe81ca8bc81786a81f47e
Tioma BOSENKO <tiomabosenko@o-s-i.org>
2023-07-21 12:14:00 +0300

    easy admin

--- src/Controller/Admin/DomainCrudController.php
+++ src/Controller/Admin/DomainCrudController.php
@@ -3,6 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Domain;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class DomainCrudController extends AbstractCrudController
@@ -12,14 +17,37 @@ class DomainCrudController extends AbstractCrudController
         return Domain::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Domain')
+            ->setEntityLabelInPlural('Domains')
+            ->setSearchFields(['name'])
+            ->setDefaultSort(['name' => 'DESC'])
+            ->showEntityActionsInlined()
+            ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('urls', 'Domain URL'))
+            // ->add(EntityFilter::new('data', 'Domain URL'))
+            // ->add(TextFilter::new('urls.date', 'Domain URL date'))
+            // ->add(TextFilter::new('urls.status', 'Domain URL status'))
+            ;
+    }
+
+    
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('name', 'Domain name');
+
+        
+        // return [
+        //     // IdField::new('id'),
+        //     TextField::new('name','Domain name'),
+        //     // TextEditorField::new('description'),
+        // ];
     }
-    */
 }
